40d665b4808e6bf79d8b1cc83ee57a62c2f1c882
Chang-Yi Lee <cy.l@inwinstack.com>
2015-06-10 14:13:56 +0800

    Minor change: Install latest Ansible from official PPA
    Ansible version must >= 1.6.0

--- install-ansible.sh
+++ install-ansible.sh
@@ -21,13 +21,9 @@ if [[ -x $(which lsb_release 2>/dev/null) ]]; then
     make install
     mkdir /etc/ansible
   elif [[ "Ubuntu" =~ $os_VENDOR ]]; then
-    if [[ "precise" =~ $os_VERSION ]]; then
-      add-apt-repository ppa:rquillo/ansible
+      add-apt-repository ppa:ansible/ansible
       apt-get update
       apt-get install -y ansible
-    else
-      apt-get install -y ansible
-    fi
   fi
 elif [[ -r /etc/redhat-release ]]; then
   yum install -y ansible
