ee61abef30723ac6ba4491a7135b8ad65e84b282
François Charlier <francois.charlier@enovance.com>
2016-01-11 17:05:53 +0100

    Fix failing install-ansible test
    Fix test for Red Hat Enterprise Server and add a default case.

--- install-ansible.sh
+++ install-ansible.sh
@@ -24,9 +24,14 @@ if [[ -x $(which lsb_release 2>/dev/null) ]]; then
     add-apt-repository ppa:ansible/ansible
     apt-get update
     apt-get install -y ansible
-  else [[ "RedHatEnterpriseServer" =~ $os_VENDOR ]]; then
+  elif [[ "RedHatEnterpriseServer" =~ $os_VENDOR ]]; then
     rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
     yum install -y ansible
+  else
+    echo "Unsupported platform ${os_VENDOR}: ${os_VERSION}"
+    echo "Please send a pull-request or open an issue"
+    echo "on https://github.com/ceph/ceph-ansible/"
+    exit 1
   fi
 elif [[ -r /etc/redhat-release ]]; then
   rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
