31811721e3c5867d8663886a367ea45642f8d18b
Sébastien Han <seb@redhat.com>
2016-04-04 15:09:44 +0200

    misc: add a script for vm test env
    Vagrant will not always be available everywhere, sometimes we want to
    bootstrap a ceph on a virtual machine. This script will help you
    bootstrapping a ceph cluster from a development branch only.
    
    So you can run it like this: ./ceph-aio-no-vagrant.sh v10.1.0
    To get the v10.1.0 version installed. If the first argument ($1) is
    empty then master will be used in consequence.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # THIS SCRIPT INSTALLS ANSIBLE
+set -e
 
 if [[ $EUID -ne 0 ]]; then
     echo "You are NOT running this script as root."
@@ -14,14 +15,14 @@ if [[ -x $(which lsb_release 2>/dev/null) ]]; then
   os_VERSION=$(lsb_release -c -s)
   if [[ "Debian" =~ $os_VENDOR ]]; then
     apt-get update
-    apt-get install python-pip python-dev git build-essential -y
+    apt-get install -y python-pip python-dev git build-essential
     pip install PyYAML jinja2 paramiko
     git clone https://github.com/ansible/ansible.git
     cd ansible
     make install
     mkdir /etc/ansible
   elif [[ "Ubuntu" =~ $os_VENDOR ]]; then
-    add-apt-repository ppa:ansible/ansible
+    add-apt-repository -y ppa:ansible/ansible
     apt-get update
     apt-get install -y ansible
   elif [[ "RedHatEnterpriseServer" =~ $os_VENDOR ]]; then
