9d9f17d7181d9ee5d400436480fe57ff53485e3d
Erwan Velu <erwan@redhat.com>
2016-11-09 17:23:11 +0100

    choose_disk: initial commit
    When ceph-ansible needs to choose disks on hosts it currently use the short
    disk path like /dev/sdb,/dev/sdc, ...
    
    This naming is a big issue as :
    - its not consistent over time : adding|removing a disk or just rebooting can change the path
    - it doesn't insure the features we are looking for : Does /dev/sdb is the 2TB
      disk we are looking for ? Does /dev/sdc is my SSD ?
    - Declaring multiple devices is pretty verbose : Selecting 5 disks means adding
      5 /dev/sd* entry
    
    Using /dev/disk/by-id/ paths instead of /dev/sd* was a first idea but prooved to
    be wrong as :
    - the path usually includes the serial number of the device making the disk listing long & painful
    - some hosts provides a pretty unreadable name like scsi-36848f690e68a50001bbef0a0094625c1
    
    So we need a different approach that is
    - easier to use (no need to know the device path)
    - dense (adding similar devices type should make a short list)
    - descriptive (select devices by their features)
    - consistent (a reboot or a disk change should not affect the result)
    
    This patch is adding a new description language to select disk's devices.
    It also includes the associated tests to perform the unitary tests
    
    This first commit introduce the main idea and a basic implementation.
    Following features & improvment will be added by further commits
    
    A Python Ansible module is added and called as a new task like :
    tasks:
        # If we can't get python2 installed before any module is used we will fail
        # so just try what we can to get it installed
        - name: gathering facts
          setup:
        - name: choose disk
          choose_disk:
            facts: "{{ansible_devices}}"
            disks: "{{lookup_disks}}"
    
    The ansible_devices is provided by Ansible once the setup call is performed : it can be manually run by
     [root@host]: ansible localhost -m setup -a filter="ansible_devices"
    
    The lookup_disks variable is adjusted by users to filter the devices they are interested in using a syntax like :
        {'storage_name': { 'feature_1': 'value_1', 'feature_2': 'value_2', 'count': number_of_devices_we_search }}
    
    A typical example is :
        vars:
          lookup_disks: "{'storage_disks': {'model': 'SAMSUNG MZ7LN512', 'rotational': '1', 'count': 2 }}"
    
    This sample description reports we are looking for 2 HDDs of that M27LN512 type.
    
    All the feature names & values are the one reported in 'ansible_devices' except
    the "count" which is a specific one of our own to indicate the number of devices
    of that type we want.
    
    As a result, a new fact is populated with the associated devices.
    
    Signed-off-by: Erwan Velu <erwan@redhat.com>

---
+++ tests/test_choose_disk.py
@@ -0,0 +1,108 @@
+import imp
+import inspect
+import library.choose_disk as choose_disk
+import mock
+import os
+from nose.tools import assert_equals, nottest
+
+host_dir = "tests/unit/hosts/"
+
+@nottest
+def get_hosts_to_test():
+    # Retrieve what function called us
+    caller = inspect.stack()[1][3]
+
+    hosts = []
+    # List all the possible modules
+    for host in os.listdir(host_dir):
+        if host.endswith(".py"):
+            # Load the module
+            host_file = imp.load_source(host, host_dir + host)
+            # If the module have the associated pepare_ function, return it
+            if (hasattr(host_file, "prepare_" + caller)):
+                hosts.append(host_file)
+    return hosts
+
+
+def test_expand_disks_explict_count_1():
+    """
+    expand_disks - test expand disk with explicit count=1
+    """
+    result = choose_disk.expand_disks(eval("{'storage_disks': {'model': 'SAMSUNG MZ7LN512', 'rotational': '1', 'count': 1 }}"))
+    assert_equals(result, eval("{'storage_disks_0': {'model': 'SAMSUNG MZ7LN512', 'rotational': '1'}}"))
+
+
+def test_expand_disks_explict_count_2():
+    """
+    expand_disks - test expand disk with explicit count=2
+    """
+    result = choose_disk.expand_disks(eval("{'storage_disks': {'model': 'SAMSUNG MZ7LN512', 'rotational': '1', 'count': 2 }}"))
+    assert_equals(result, eval("{'storage_disks_0': {'model': 'SAMSUNG MZ7LN512', 'rotational': '1'}, 'storage_disks_1': {'model': 'SAMSUNG MZ7LN512', 'rotational': '1'}}"))
+
+
+def test_expand_disks_implicit_count():
+    """
+    expand_disks - test expand disk with implicit count=1
+    """
+    result = choose_disk.expand_disks(eval("{'storage_disks': {'model': 'SAMSUNG MZ7LN512', 'rotational': '1'}}"))
+    assert_equals(result, eval("{'storage_disks_0': {'model': 'SAMSUNG MZ7LN512', 'rotational': '1'}}"))
+
+
+def test_expand_disks_multiple_implicit_count():
+    """
+    expand_disks - test expand disk with two implicit count=1
+    """
+    result = choose_disk.expand_disks(eval("{'storage_disks': {'model': 'SAMSUNG MZ7LN512', 'rotational': '1'},\
+                                             'journal_disks': {'model': 'SANDISK PLIPM512', 'rotational': '0'}}"))
+    assert_equals(result, eval("{'journal_disks_0': {'model': 'SANDISK PLIPM512', 'rotational': '0'},\
+                                 'storage_disks_0': {'model': 'SAMSUNG MZ7LN512', 'rotational': '1'}}"))
+
+
+def test_find_match_matched():
+    """
+    find_match - test for a matching device
+    """
+    for host_file in get_hosts_to_test():
+        disk_0, expected_result = host_file.prepare_test_find_match_matched()
+        result = choose_disk.find_match(host_file.ansible_devices, disk_0)
+        assert_equals(result, expected_result)
+
+
+def test_find_match_unmatched():
+    """
+    find_match - test for a non-matching device
+    """
+    for host_file in get_hosts_to_test():
+        disk_0, expected_result = host_file.prepare_test_find_match_unmatched()
+        result = choose_disk.find_match(host_file.ansible_devices, disk_0)
+        assert_equals(result, expected_result)
+
+
+def test_select_only_free_devices():
+    """
+    select_only_free_devices - Removing devices that have partitions
+    """
+
+    for host_file in get_hosts_to_test():
+        expected_result = host_file.prepare_test_select_only_free_devices()
+        result = choose_disk.select_only_free_devices(host_file.ansible_devices)
+        assert_equals(result, expected_result)
+
+
+def test_get_block_devices_persistent_name():
+    """
+    get_block_devices_persistent_name - Looking persistent name for listed block devices
+    """
+    for host_file in get_hosts_to_test():
+        disk_facts, expected_result = host_file.prepare_test_get_block_devices_persistent_name()
+
+        def fake_readlink(name):
+            return host_file.disk_by_id[name.split('/')[-1]]
+
+        with mock.patch('library.choose_disk.os') as mocked_os:
+            mocked_os.readlink.side_effect = fake_readlink
+            mocked_os.listdir.return_value = host_file.disk_by_id.keys()
+            result = choose_disk.get_block_devices_persistent_name(disk_facts)
+            assert_equals(result, expected_result)
+
+# test_choosedisk.py ends here
---
+++ tests/unit/hosts/ThinkPad_W541.py
@@ -0,0 +1,43 @@
+ansible_devices = {
+        'sr0': {'scheduler_mode': 'cfq', 'sectorsize': '512', 'vendor': 'MATSHITA', 'sectors': '2097151', 'sas_device_handle': None, 'sas_address': None,
+            'host': 'SATA controller: Intel Corporation 8 Series/C220 Series Chipset Family 6-port SATA Controller 1 [AHCI mode] (rev 04)',
+            'rotational': '1', 'removable': '1', 'support_discard': '0', 'holders': [],
+            'size': '1024.00 MB', 'model': 'DVD-RAM UJ8G2', 'partitions': {}
+            },
+        'sda': {'scheduler_mode': 'cfq', 'sectorsize': '512', 'vendor': 'ATA',
+            'sectors': '1000215216', 'sas_device_handle': None, 'sas_address': None,
+            'host': 'SATA controller: Intel Corporation 8 Series/C220 Series Chipset Family 6-port SATA Controller 1 [AHCI mode] (rev 04)',
+            'rotational': '0', 'removable': '0', 'support_discard': '512', 'holders': [], 'size': '476.94 GB',
+            'model': 'SAMSUNG MZ7LN512',
+            'partitions': {
+                'sda4': {'sectorsize': 512, 'uuid': None, 'sectors': '2', 'start': '663115776', 'holders': [], 'size': '1.00 KB'},
+                'sda5': {'sectorsize': 512, 'uuid': 'daddc2c4-1463-4d46-abc7-b15770b79f94', 'sectors': '337096704', 'start': '663117824', 'holders': ['luks-daddc2c4-1463-4d46-abc7-b15770b79f94'], 'size': '160.74 GB'},
+                'sda2': {'sectorsize': 512, 'uuid': 'c3d3ae17-1ece-432e-bb4b-f38bfa12e876', 'sectors': '629149696', 'start': '411648', 'holders': [], 'size': '300.00 GB'},
+                'sda3': {'sectorsize': 512, 'uuid': '51f0126d-0d7f-4708-b8b8-f122ac8dcf46', 'sectors': '33554432',  'start': '629561344', 'holders': [], 'size': '16.00 GB'},
+                'sda1': {'sectorsize': 512, 'uuid': '71adc8ce-fc61-419d-82d5-0faec6d97f60', 'sectors': '409600', 'start': '2048', 'holders': [], 'size': '200.00 MB'}
+                }
+            }
+        }
+
+
+def prepare_test_find_match_matched():
+    disk_0 = {'storage_disks_0': {'model': 'SAMSUNG MZ7LN512', 'rotational': '0'}}
+    expected_result = {
+            'sda': {'sectorsize': '512', 'vendor': 'ATA', 'sas_device_handle': None, 'host': 'SATA controller: Intel Corporation 8 Series/C220 Series Chipset Family 6-port SATA Controller 1 [AHCI mode] (rev 04)',
+                'support_discard': '512', 'model': 'SAMSUNG MZ7LN512', 'size': '476.94 GB', 'scheduler_mode': 'cfq', 'rotational': '0', 'sectors': '1000215216', 'sas_address':  None,
+                'removable': '0', 'holders': [],
+                'partitions': {
+                    'sda4': {'sectorsize': 512, 'uuid': None, 'sectors': '2', 'start': '663115776', 'holders': [], 'size': '1.00 KB'},
+                    'sda5': {'sectorsize': 512, 'uuid': 'daddc2c4-1463-4d46-abc7-b15770b79f94', 'sectors': '337096704', 'start': '663117824', 'holders': ['luks-daddc2c4-1463-4d46-abc7-b15770b79f94'], 'size': '160.74 GB'},
+                    'sda2': {'sectorsize': 512, 'uuid': 'c3d3ae17-1ece-432e-bb4b-f38bfa12e876', 'sectors': '629149696', 'start': '411648', 'holders': [], 'size': '300.00 GB'},
+                    'sda3': {'sectorsize': 512, 'uuid': '51f0126d-0d7f-4708-b8b8-f122ac8dcf46', 'sectors': '33554432', 'start': '629561344', 'holders': [], 'size': '16.00 GB'},
+                    'sda1': {'sectorsize': 512, 'uuid': '71adc8ce-fc61-419d-82d5-0faec6d97f60', 'sectors': '409600', 'start': '2048', 'holders': [], 'size': '200.00 MB'}
+                    }
+                }
+            }
+
+    return (disk_0, expected_result)
+
+
+def prepare_test_find_match_unmatched():
+    return({'storage_disks_0': {'model': 'SAMSUNG MZ7LN512', 'rotational': '1'}}, {})
