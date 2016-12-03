b5552239e750721428cfaad7d729f674ac6edfd1
Andrew Schoen <aschoen@redhat.com>
2016-12-03 08:10:54 -0600

    tests: port tests/functional/test_install.py to use testinfra fixtures
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- tests/functional/tests/test_install.py
+++ tests/functional/tests/test_install.py
@@ -1,10 +1,17 @@
-import os
-
 
 class TestInstall(object):
 
-    def test_ceph_dir_exists(self):
-        assert os.path.isdir('/etc/ceph')
+    def test_ceph_dir_exists(self, File):
+        assert File('/etc/ceph').exists
 
-    def test_ceph_conf_exists(self):
-        assert os.path.isfile('/etc/ceph/ceph.conf')
+    def test_ceph_dir_is_a_directory(self, File):
+        assert File('/etc/ceph').is_directory
+
+    def test_ceph_conf_exists(self, File):
+        assert File('/etc/ceph/ceph.conf').exists
+
+    def test_ceph_conf_is_a_file(self, File):
+        assert File('/etc/ceph/ceph.conf').is_file
+
+    def test_ceph_command_exists(self, Command):
+        assert Command.exists("ceph")
