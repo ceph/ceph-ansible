4effce1c31c63fb2a44f4c56bdb3ef881ba1eba3
Andrew Schoen <aschoen@redhat.com>
2016-12-06 13:39:08 -0600

    tests: use node["conf_path"] instead of hardcoding the path in tests
    This also accounts for the cluster having a custom cluster name
    
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- tests/functional/tests/test_install.py
+++ tests/functional/tests/test_install.py
@@ -7,11 +7,11 @@ class TestInstall(object):
     def test_ceph_dir_is_a_directory(self, File):
         assert File('/etc/ceph').is_directory
 
-    def test_ceph_conf_exists(self, File):
-        assert File('/etc/ceph/ceph.conf').exists
+    def test_ceph_conf_exists(self, File, node):
+        assert File(node["conf_path"]).exists
 
-    def test_ceph_conf_is_a_file(self, File):
-        assert File('/etc/ceph/ceph.conf').is_file
+    def test_ceph_conf_is_a_file(self, File, node):
+        assert File(node["conf_path"]).is_file
 
     def test_ceph_command_exists(self, Command):
         assert Command.exists("ceph")
@@ -20,20 +20,20 @@ class TestInstall(object):
 class TestCephConf(object):
 
     def test_ceph_config_has_inital_members_line(self, node, File):
-        assert File("/etc/ceph/ceph.conf").contains("^mon initial members = .*$")
+        assert File(node["conf_path"]).contains("^mon initial members = .*$")
 
     def test_initial_members_line_has_correct_value(self, node, File):
         mons = ",".join("ceph-%s" % host
                         for host in node["vars"]["groups"]["mons"])
         line = "mon initial members = {}".format(mons)
-        assert File("/etc/ceph/ceph.conf").contains(line)
+        assert File(node["conf_path"]).contains(line)
 
     def test_ceph_config_has_mon_host_line(self, node, File):
-        assert File("/etc/ceph/ceph.conf").contains("^mon host = .*$")
+        assert File(node["conf_path"]).contains("^mon host = .*$")
 
     def test_mon_host_line_has_correct_value(self, node, File):
         mon_ips = []
         for x in range(0, node["num_mons"]):
             mon_ips.append("{}.1{}".format(node["subnet"], x))
         line = "mon host = {}".format(",".join(mon_ips))
-        assert File("/etc/ceph/ceph.conf").contains(line)
+        assert File(node["conf_path"]).contains(line)
