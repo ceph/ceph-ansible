d874e0c95b28fa8ea0f85210bc0de17a6fd19583
Erwan Velu <erwan@redhat.com>
2017-02-15 11:27:12 +0100

    choose_disk: Reporting empty {legacy|storage}_devices when not used
    As we do mix both variable in some playbooks, let's report them empty if
    not used to avoid an error reporting that variable doesn't exist.

--- library/choose_disk.py
+++ library/choose_disk.py
@@ -627,10 +627,10 @@ def main():
 
     if legacy is True:
         # Reporting devices & raw_journal_devices for compatiblity
-        module.exit_json(msg=message, changed=changed, ansible_facts=dict(legacy_devices=ceph_data, journal_devices=journal, devices_to_activate=to_activate))
+        module.exit_json(msg=message, changed=changed, ansible_facts=dict(legacy_devices=ceph_data, journal_devices=journal, devices_to_activate=to_activate, storage_devices=[]))
     else:
         # Reporting storage_devices & journal_devices
-        module.exit_json(msg=message, changed=changed, ansible_facts=dict(storage_devices=ceph_data, journal_devices=journal, devices_to_activate=to_activate))
+        module.exit_json(msg=message, changed=changed, ansible_facts=dict(storage_devices=ceph_data, journal_devices=journal, devices_to_activate=to_activate, legacy_devices=[]))
 
 
 if __name__ == '__main__':
