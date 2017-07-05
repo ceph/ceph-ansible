476d1677eb675ff09164fd284412698bc804aea7
Sébastien Han <seb@redhat.com>
2017-07-05 10:45:25 +0200

    tests: allow bluestore devices
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- tests/functional/tests/osd/test_journal_collocation.py
+++ tests/functional/tests/osd/test_journal_collocation.py
@@ -4,4 +4,4 @@ class TestOSD(object):
     def test_osds_are_all_collocated(self, node, Command):
         # TODO: figure out way to paramaterize node['vars']['devices'] for this test
         for device in node["vars"]["devices"]:
-            assert Command.check_output("sudo blkid -s PARTLABEL -o value %s2" % device) == "ceph journal"
+            assert Command.check_output("sudo blkid -s PARTLABEL -o value %s2" % device) in ["ceph journal", "ceph block"]
