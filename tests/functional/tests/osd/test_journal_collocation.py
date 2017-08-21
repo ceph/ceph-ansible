75060119c14ee40c51a21effc13bc7e449989514
Alfredo Deza <adeza@redhat.com>
2017-08-21 15:42:46 -0400

    tests/osd: update tests to use new host fixture
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- tests/functional/tests/osd/test_journal_collocation.py
+++ tests/functional/tests/osd/test_journal_collocation.py
@@ -1,7 +1,7 @@
 
 class TestOSD(object):
 
-    def test_osds_are_all_collocated(self, node, Command):
+    def test_osds_are_all_collocated(self, node, host):
         # TODO: figure out way to paramaterize node['vars']['devices'] for this test
         for device in node["vars"]["devices"]:
-            assert Command.check_output("sudo blkid -s PARTLABEL -o value %s2" % device) in ["ceph journal", "ceph block"]
+            assert host.check_output("sudo blkid -s PARTLABEL -o value %s2" % device) in ["ceph journal", "ceph block"]
