05e546bce7d42ba46a7246b1e085af8ab259c2e3
Andrew Schoen <aschoen@redhat.com>
2018-04-12 15:14:22 -0500

    wip commit
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- plugins/actions/validate.py
+++ plugins/actions/validate.py
@@ -1,12 +1,9 @@
-import sys
-import os
 
 from ansible.plugins.action import ActionBase
 
 import notario
 from notario.exceptions import Invalid
-from notario.decorators import optional
-from notario.validators import types, recursive
+from notario.validators import types, chainable
 
 try:
     from __main__ import display
@@ -18,8 +15,10 @@ except ImportError:
 class ActionModule(ActionBase):
 
     def run(self, tmp=None, task_vars=None):
-        # we must use hostvars, since task_vars will have un-processed variables
-        hostvars = task_vars['hostvars']
+        # we must use vars, since task_vars will have un-processed variables
+        host_vars = task_vars['vars']
+        display.warning(host_vars)
+        host = host_vars['ansible_hostname']
         mode = self._task.args.get('mode', 'permissive')
 
         self._supports_check_mode = False # XXX ?
@@ -28,31 +27,34 @@ class ActionModule(ActionBase):
         result = {}
         result['_ansible_verbose_always'] = True
 
-        for host, _vars in hostvars.items():
-            try:
-                notario.validate(_vars, install_options, defined_keys=True)
-                if _vars["ceph_origin"] == "repository":
-                    notario.validate(_vars, ceph_origin_repository, defined_keys=True)
+        try:
+            notario.validate(host_vars, install_options, defined_keys=True)
 
-                    if _vars["ceph_repository"] == "community":
-                        notario.validate(_vars, ceph_repository_community, defined_keys=True)
+            if host_vars["ceph_origin"] == "repository":
+                notario.validate(host_vars, ceph_origin_repository, defined_keys=True)
 
-                    if _vars["ceph_repository"] == "rhcs":
-                        notario.validate(_vars, ceph_repository_rhcs, defined_keys=True)
+                if host_vars["ceph_repository"] == "community":
+                    notario.validate(host_vars, ceph_repository_community, defined_keys=True)
 
+                if host_vars["ceph_repository"] == "rhcs":
+                    notario.validate(host_vars, ceph_repository_rhcs, defined_keys=True)
 
-            except Invalid as error:
-                display.vvvv("Notario Failure: %s" % str(error))
-                display.warning("[%s] Validation failed for variable: %s" % (host, error.path[0]))
-                msg = "Invalid variable assignment in host: %s\n" % host
-                msg += "    %s = %s\n" % (error.path[0], error.path[1])
-                msg += "    %s   %s\n" % (" " * len(str(error.path[0])), "^" * len(str(error.path[1])))
-                msg += "Reason: %s" % error.reason
-                result['failed'] = mode == 'strict'
-                result['msg'] = msg
-                result['stderr_lines'] = msg.split('\n')
+                if host_vars["ceph_repository"] == "dev":
+                    notario.validate(host_vars, ceph_repository_dev, defined_keys=True)
 
-            return result
+        except Invalid as error:
+            display.vvvv("Notario Failure: %s" % str(error))
+            display.warning(error)
+            display.warning("[%s] Validation failed for variable: %s" % (host, error.path))
+            msg = "Invalid variable assignment in host: %s\n" % host
+            msg += "    %s = %s\n" % (error.path[0], error.path[1])
+            msg += "    %s   %s\n" % (" " * len(str(error.path[0])), "^" * len(str(error.path[1])))
+            msg += "Reason: %s" % error.reason
+            result['failed'] = mode == 'strict'
+            result['msg'] = msg
+            result['stderr_lines'] = msg.split('\n')
+
+        return result
 
 # Schemas
 
@@ -78,19 +80,21 @@ install_options = (
     ('osd_objectstore', osd_objectstore_choices),
 )
 
-ceph_origin_repository = (
-    ("ceph_repository", ceph_repository_choices),
-
-)
+ceph_origin_repository = ("ceph_repository", ceph_repository_choices)
 
 ceph_repository_community = (
     ("ceph_mirror", types.string),
     ("ceph_stable_key", types.string),
-    ("ceph_stable_release", types.string),  # do we need to make sure this is not 'dummy'?
+    ("ceph_stable_release", types.string),
     ("ceph_stable_repo", types.string),
 )
 
 ceph_repository_rhcs = (
     ("ceph_repository_type", ceph_repository_type_choices),
-    ("ceph_rchs_version", types.string),  # FIXME: this might also be a integer
+    ("ceph_rhcs_version", chainable.AnyIn(types.string, types.integer)),
+)
+
+ceph_repository_dev = (
+    ("ceph_dev_branch", types.string),
+    ("ceph_dev_sha1", types.string),
 )
