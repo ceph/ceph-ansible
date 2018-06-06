2a17480fe4b4e84d2e1b274faac81e79d7f9d1f8
Sébastien Han <seb@redhat.com>
2018-06-06 12:22:16 +0800

    test_mds: fix linting issues
    Make linter happy and remove unused variable.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- tests/functional/tests/mds/test_mds.py
+++ tests/functional/tests/mds/test_mds.py
@@ -1,6 +1,7 @@
 import pytest
 import json
 
+
 class TestMDSs(object):
 
     @pytest.mark.no_docker
@@ -22,19 +23,19 @@ class TestMDSs(object):
     @pytest.mark.no_docker
     def test_mds_is_up(self, node, host):
         hostname = node["vars"]["inventory_hostname"]
-        cmd = "sudo ceph --name client.bootstrap-mds --keyring /var/lib/ceph/bootstrap-mds/{cluster}.keyring --cluster={cluster} --connect-timeout 5 -f json -s".format(cluster=node['cluster_name'])
+        cmd = "sudo ceph --name client.bootstrap-mds --keyring /var/lib/ceph/bootstrap-mds/{cluster}.keyring --cluster={cluster} --connect-timeout 5 -f json -s".format(  # noqa E501
+            cluster=node['cluster_name'])
         output = host.check_output(cmd)
         daemons = json.loads(output)["fsmap"]["by_rank"][0]["name"]
         assert hostname in daemons
 
     @pytest.mark.docker
     def test_docker_mds_is_up(self, node, host):
-        hostname = node["vars"]["inventory_hostname"]
-        cmd = "sudo docker exec ceph-mds-{hostname} ceph --name client.bootstrap-mds --keyring /var/lib/ceph/bootstrap-mds/{cluster}.keyring --cluster={cluster} --connect-timeout 5 -f json -s".format(
+        cmd = "sudo docker exec ceph-mds-{hostname} ceph --name client.bootstrap-mds --keyring /var/lib/ceph/bootstrap-mds/{cluster}.keyring --cluster={cluster} --connect-timeout 5 -f json -s".format(  # noqa E501
             hostname=node["vars"]["inventory_hostname"],
             cluster=node["cluster_name"]
         )
         num_mdss = len(host.ansible.get_variables()["groups"]["mdss"])
         output_raw = host.check_output(cmd)
         output_json = json.loads(output_raw)
-        assert output_json['fsmap']['up'] and output_json['fsmap']['in'] == num_mdss
+        assert output_json['fsmap']['up'] and output_json['fsmap']['in'] == num_mdss  # noqa E501
