be9a28d4f56110c6658cdf2dff1bb38a1b874350
Guillaume Abrioux <gabrioux@redhat.com>
2018-06-25 17:10:37 +0200

    tests: factorize docker tests using docker_exec_cmd logic
    avoid duplicating test unnecessarily just because of docker exec syntax.
    Using the same logic than in the playbook with `docker_exec_cmd` allow us
    to execute the same test on both containerized and non containerized environment.
    
    The idea is to set a variable `docker_exec_cmd` with the
    'docker exec <container-name>' string when containerized and
    set it to '' when non containerized.
    
    Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>
    (cherry picked from commit f2e57a56db2801818135ba85479fedfc00eae30c)

--- tests/functional/tests/mds/test_mds.py
+++ tests/functional/tests/mds/test_mds.py
@@ -19,21 +19,16 @@ class TestMDSs(object):
         )
         assert host.service(service_name).is_enabled
 
-    @pytest.mark.no_docker
     def test_mds_is_up(self, node, host):
         hostname = node["vars"]["inventory_hostname"]
-        cmd = "sudo ceph --name client.bootstrap-mds --keyring /var/lib/ceph/bootstrap-mds/{cluster}.keyring --cluster={cluster} --connect-timeout 5 -f json -s".format(cluster=node['cluster_name'])
-        cluster_status = json.loads(host.check_output(cmd))
+        if node['docker']:
+            docker_exec_cmd = 'docker exec ceph-mds-{hostname}'.format(hostname=hostname)
+        else:
+            docker_exec_cmd = ''
 
-        assert (cluster_status['fsmap'].get('up', 0) + cluster_status['fsmap'].get('up:standby', 0)) == len(node["vars"]["groups"]["mdss"])
-
-    @pytest.mark.docker
-    def test_docker_mds_is_up(self, node, host):
-        hostname = node["vars"]["inventory_hostname"]
-        cmd = "sudo docker exec ceph-mds-{hostname} ceph --name client.bootstrap-mds --keyring /var/lib/ceph/bootstrap-mds/{cluster}.keyring --cluster={cluster} --connect-timeout 5 -f json -s".format(
-            hostname=node["vars"]["inventory_hostname"],
-            cluster=node["cluster_name"]
+        cmd = "sudo {docker_exec_cmd} ceph --name client.bootstrap-mds --keyring /var/lib/ceph/bootstrap-mds/{cluster}.keyring --cluster={cluster} --connect-timeout 5 -f json -s".format(
+            docker_exec_cmd=docker_exec_cmd,
+            cluster=node['cluster_name']
         )
         cluster_status = json.loads(host.check_output(cmd))
-
         assert (cluster_status['fsmap'].get('up', 0) + cluster_status['fsmap'].get('up:standby', 0)) == len(node["vars"]["groups"]["mdss"])
