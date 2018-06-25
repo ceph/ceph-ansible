f0f83b223fda7c681f4941d34934e3d3e72896b2
Guillaume Abrioux <gabrioux@redhat.com>
2018-06-25 17:10:37 +0200

    tests: factorize docker tests using docker_exec_cmd logic
    avoid duplicating test unnecessarily just because of docker exec syntax.
    Using the same logic than in the playbook with `docker_exec_cmd` allow us
    to execute the same test on both containerized and non containerized environment.
    
    The idea is to set a variable `docker_exec_cmd` with the
    'docker exec <container-name>' string when containerized and
    set it to '' when non containerized.
    
    Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

--- tests/functional/tests/nfs/test_nfs_ganesha.py
+++ tests/functional/tests/nfs/test_nfs_ganesha.py
@@ -23,23 +23,15 @@ class TestNFSs(object):
     def test_nfs_config_override(self, node, host):
         assert host.file("/etc/ganesha/ganesha.conf").contains("Entries_HWMark")
 
-    @pytest.mark.no_docker
     def test_nfs_is_up(self, node, host):
         hostname = node["vars"]["inventory_hostname"]
         cluster = node['cluster_name']
-        cmd = "sudo ceph --name client.rgw.{hostname} --keyring /var/lib/ceph/radosgw/{cluster}-rgw.{hostname}/keyring --cluster={cluster} --connect-timeout 5 -f json -s".format(
-            hostname=hostname,
-            cluster=cluster
-        )
-        output = host.check_output(cmd)
-        daemons = [i for i in json.loads(output)["servicemap"]["services"]["rgw-nfs"]["daemons"]]
-        assert hostname in daemons
-
-    @pytest.mark.docker
-    def test_docker_nfs_is_up(self, node, host):
-        hostname = node["vars"]["inventory_hostname"]
-        cluster = node['cluster_name']
-        cmd = "sudo docker exec ceph-nfs-{hostname} ceph --name client.rgw.{hostname} --keyring /var/lib/ceph/radosgw/{cluster}-rgw.{hostname}/keyring --cluster={cluster} --connect-timeout 5 -f json -s".format(
+        if node['docker']:
+            docker_exec_cmd = 'docker exec ceph-nfs-{hostname}'.format(hostname=hostname)
+        else:
+            docker_exec_cmd = ''
+        cmd = "sudo {docker_exec_cmd} ceph --name client.rgw.{hostname} --keyring /var/lib/ceph/radosgw/{cluster}-rgw.{hostname}/keyring --cluster={cluster} --connect-timeout 5 -f json -s".format(
+            docker_exec_cmd=docker_exec_cmd,
             hostname=hostname,
             cluster=cluster
         )
