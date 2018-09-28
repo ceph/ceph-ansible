2da338c4c5e08af5339c71ce25a737dbe0fc358c
Sébastien Han <seb@redhat.com>
2018-09-28 13:06:18 +0200

    ceph_volume: add container support for batch command
    The batch option got recently added, while rebasing this patch it was
    necessary to implement it. So now, the batch option can work on
    containerized environments.
    
    Closes: https://bugzilla.redhat.com/show_bug.cgi?id=1630977
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- library/ceph_volume.py
+++ library/ceph_volume.py
@@ -252,20 +252,21 @@ def batch(module):
     journal_size = module.params['journal_size']
     block_db_size = module.params['block_db_size']
     report = module.params['report']
+    subcommand = 'batch'
 
     if not batch_devices:
         module.fail_json(
             msg='batch_devices must be provided if action is "batch"', changed=False, rc=1)  # noqa 4502
 
-    cmd = [
-        'ceph-volume',
-        '--cluster',
-        cluster,
-        'lvm',
-        'batch',
-        '--%s' % objectstore,
-        '--yes',
-    ]
+    if "CEPH_CONTAINER_IMAGE" in os.environ:
+        container_image = os.getenv("CEPH_CONTAINER_IMAGE")
+    else:
+        container_image = None
+
+    cmd = ceph_volume_cmd(subcommand, container_image, cluster)
+    cmd.extend(["--%s" % objectstore])
+    cmd.extend("--yes")
+    cmd.extend("--no-systemd ")
 
     if crush_device_class:
         cmd.extend(["--crush-device-class", crush_device_class])
