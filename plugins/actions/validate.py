ceph/ceph-ansible

commit 5aa27794615e7d4521b1dbf1444b61388aacb852

validate: fix notario error
Typical error:

```
AttributeError: 'Invalid' object has no attribute 'message'
```

As of python 2.6, `BaseException.message` has been deprecated.
When using python3, it fails because it has been removed.

Let's use `str(error)` instead so we don't hit this error when using
python3.

Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

plugins/actions/validate.py
@@ -111,7 +111,7 @@ class ActionModule(ActionBase):
             display.error(msg)
             reason = "[{}] Reason: {}".format(host, error.reason)
             try:
-                if "schema is missing" not in error.message:
+                if "schema is missing" not str(error):
                     for i in range(0, len(error.path)):
                         if i == 0:
                             given = "[{}] Given value for {}".format(

commit 5e6675110315b832d74167659e2324d2ddfd0962

validate: fix a typo
5aa27794615e7d4521b1dbf1444b61388aacb852 introduced a typo.
This commit fixes it.

Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

plugins/actions/validate.py
@@ -111,7 +111,7 @@ class ActionModule(ActionBase):
             display.error(msg)
             reason = "[{}] Reason: {}".format(host, error.reason)
             try:
-                if "schema is missing" not str(error):
+                if "schema is missing" not in str(error):
                     for i in range(0, len(error.path)):
                         if i == 0:
                             given = "[{}] Given value for {}".format(