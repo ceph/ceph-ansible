2dee06f14f092a305ed0f3e0ee1194b49089de8c
Dimitri Savineau <dsavinea@redhat.com>
2020-02-17 15:46:54 -0500

    ceph-dashboard: update create/get rgw user tasks
    Since [1] if a rgw user already exists then the radosgw-admin user create
    command will return an error instead of modifying the current user.
    We were already doing separated tasks for create and get operation but
    only for multisite configuration but it's not enough.
    Instead we should do the get task first and depending on the result
    execute the create.
    This commit also adds missing run_once and delegate_to statement.
    
    [1] https://github.com/ceph/ceph/commit/269e9b9
    
    Signed-off-by: Dimitri Savineau <dsavinea@redhat.com>

--- tests/functional/tests/rgw/test_rgw_tuning.py
+++ tests/functional/tests/rgw/test_rgw_tuning.py
@@ -13,12 +13,18 @@ class TestRGWs(object):
 
     @pytest.mark.no_docker
     def test_rgw_bucket_default_quota_is_applied(self, node, host, setup):
-        radosgw_admin_cmd = "sudo radosgw-admin --cluster={cluster} -n client.rgw.{hostname}.rgw0 --keyring /var/lib/ceph/radosgw/{cluster}-rgw.{hostname}.rgw0/keyring user create --uid=test --display-name Test".format(  # noqa E501
+        radosgw_admin_cmd = "sudo radosgw-admin --cluster={cluster} -n client.rgw.{hostname}.rgw0 --keyring /var/lib/ceph/radosgw/{cluster}-rgw.{hostname}.rgw0/keyring user info --uid=test".format(  # noqa E501
             hostname=node["vars"]["inventory_hostname"],
             cluster=setup['cluster_name']
         )
-        radosgw_admin_output = host.check_output(radosgw_admin_cmd)
-        radosgw_admin_output_json = json.loads(radosgw_admin_output)
+        radosgw_admin_output = host.run(radosgw_admin_cmd)
+        if radosgw_admin_output.rc == 22:
+            radosgw_admin_cmd = "sudo radosgw-admin --cluster={cluster} -n client.rgw.{hostname}.rgw0 --keyring /var/lib/ceph/radosgw/{cluster}-rgw.{hostname}.rgw0/keyring user create --uid=test --display-name Test".format(  # noqa E501
+                hostname=node["vars"]["inventory_hostname"],
+                cluster=setup['cluster_name']
+            )
+            radosgw_admin_output = host.run(radosgw_admin_cmd)
+        radosgw_admin_output_json = json.loads(radosgw_admin_output.stdout)
         assert radosgw_admin_output_json["bucket_quota"]["enabled"] == True  # noqa E501
         assert radosgw_admin_output_json["bucket_quota"]["max_objects"] == 1638400  # noqa E501
 
