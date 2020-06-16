72293b6614c0fd83fe317001f0990098cef6efc0
Dimitri Savineau <dsavinea@redhat.com>
2020-06-16 15:31:08 -0400

    vagrant: update centos image to 8.2
    CentOS 8.2 (2004) has been relesed so we should switch to this image
    when using vagrant.
    
    Signed-off-by: Dimitri Savineau <dsavinea@redhat.com>

--- tests/scripts/vagrant_up.sh
+++ tests/scripts/vagrant_up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 vagrant box remove --provider libvirt --box-version 1905.1 centos/8 || true
-vagrant box add --provider libvirt --name centos/8 https://cloud.centos.org/centos/8/vagrant/x86_64/images/CentOS-8-Vagrant-8.1.1911-20200113.3.x86_64.vagrant-libvirt.box || true
+vagrant box add --provider libvirt --name centos/8 https://cloud.centos.org/centos/8/vagrant/x86_64/images/CentOS-8-Vagrant-8.2.2004-20200611.2.x86_64.vagrant-libvirt.box || true
 
 retries=0
 until [ $retries -ge 5 ]
