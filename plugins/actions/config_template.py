93c5862f6eb0543c0ff51f3a1c28ea5d6352736f
Bhagyashri Shewale <bshewale@redhat.com>
2020-06-19 17:16:09 +0530

    [DNM] testing rdo git workflow
    Signed-off-by: Bhagyashri Shewale <bshewale@redhat.com>

--- plugins/actions/config_template.py
+++ plugins/actions/config_template.py
@@ -12,6 +12,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
+#
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
