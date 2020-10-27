9ee44013c51fa71697145514c9165b11a64446f4
Dimitri Savineau <dsavinea@redhat.com>
2020-10-27 12:14:19 -0400

    library: remove unused module import
    Move the import at the top of the file and remove unused module import.
    
    - E402 module level import not at top of file
    - F401 'xxxx' imported but unused
    
    This also removes the '# noqa E402' statement from the code.
    
    Signed-off-by: Dimitri Savineau <dsavinea@redhat.com>
    (cherry picked from commit 2138a00a3294b222d5e8325495300841ed5a7f5f)

--- library/ceph_volume.py
+++ library/ceph_volume.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from ansible.module_utils.basic import AnsibleModule
 import datetime
 import copy
 import json
@@ -183,9 +184,6 @@ EXAMPLES = '''
     wal: /dev/sdc2
     action: create
 '''
-
-
-from ansible.module_utils.basic import AnsibleModule  # noqa: E402
 
 
 def fatal(message, module):
