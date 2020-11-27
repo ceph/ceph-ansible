0fa99c9b223b37cc9edf195c9f7a6af4ae069640
Dimitri Savineau <dsavinea@redhat.com>
2020-11-27 12:25:11 -0500

    improve plugins/filter testing
    - The plugins/filter directory wasn't present in the flake8 workflow
    configuration.
    - Fix the flake8 syntax.
    - Add the directory to PYTHONPATH environment variable for pytest
    to avoid importing the plugin filter via sys.
    - Add unittest on missing netaddr module import.
    
    Signed-off-by: Dimitri Savineau <dsavinea@redhat.com>

--- tests/plugins/filter/test_ipaddrs_in_ranges.py
+++ tests/plugins/filter/test_ipaddrs_in_ranges.py
@@ -1,8 +1,15 @@
-import sys
-sys.path.append('./plugins/filter')
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.errors import AnsibleFilterError
+
 import ipaddrs_in_ranges
+import pytest
+
+pytest.importorskip('netaddr')
 
 filter_plugin = ipaddrs_in_ranges.FilterModule()
+
 
 class TestIpaddrsInRanges(object):
 
@@ -41,5 +48,16 @@ class TestIpaddrsInRanges(object):
         ips = ['10.10.20.1', '192.168.2.1', '172.16.10.1']
         ranges = ['192.168.1.0/24', '10.10.10.1/24', '172.16.17.0/24']
         result = filter_plugin.ips_in_ranges(ips, ranges)
-        assert result == []
+        assert len(result) == 0
 
+    def test_ips_in_ranges_in_filters_dict(self):
+        assert 'ips_in_ranges' in filter_plugin.filters()
+
+    def test_missing_netaddr_module(self):
+        ipaddrs_in_ranges.netaddr = None
+
+        with pytest.raises(AnsibleFilterError) as result:
+            filter_plugin.filters()
+
+        assert result.type == AnsibleFilterError
+        assert str(result.value) == "The ips_in_ranges filter requires python's netaddr be installed on the ansible controller."
