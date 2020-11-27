c610a1b41179d15d58dac5b72ef39a280a8a9b6c
Dimitri Savineau <dsavinea@redhat.com>
2020-11-27 15:21:13 -0500

    tests: use ca_test_common with ceph_volume_simple
    To avoid duplicate code.
    
    Signed-off-by: Dimitri Savineau <dsavinea@redhat.com>

--- tests/library/test_ceph_volume_simple_scan.py
+++ tests/library/test_ceph_volume_simple_scan.py
@@ -1,9 +1,7 @@
 from mock.mock import patch
-from ansible.module_utils import basic
-from ansible.module_utils._text import to_bytes
-import json
 import os
 import pytest
+import ca_test_common
 import ceph_volume_simple_scan
 
 fake_cluster = 'ceph'
@@ -12,37 +10,16 @@ fake_container_image = 'quay.ceph.io/ceph/daemon:latest'
 fake_path = '/var/lib/ceph/osd/ceph-0'
 
 
-def set_module_args(args):
-    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
-    basic._ANSIBLE_ARGS = to_bytes(args)
-
-
-class AnsibleExitJson(Exception):
-    pass
-
-
-class AnsibleFailJson(Exception):
-    pass
-
-
-def exit_json(*args, **kwargs):
-    raise AnsibleExitJson(kwargs)
-
-
-def fail_json(*args, **kwargs):
-    raise AnsibleFailJson(kwargs)
-
-
 class TestCephVolumeSimpleScanModule(object):
 
-    @patch.object(basic.AnsibleModule, 'exit_json')
+    @patch('ansible.module_utils.basic.AnsibleModule.exit_json')
     def test_with_check_mode(self, m_exit_json):
-        set_module_args({
+        ca_test_common.set_module_args({
             '_ansible_check_mode': True
         })
-        m_exit_json.side_effect = exit_json
+        m_exit_json.side_effect = ca_test_common.exit_json
 
-        with pytest.raises(AnsibleExitJson) as result:
+        with pytest.raises(ca_test_common.AnsibleExitJson) as result:
             ceph_volume_simple_scan.main()
 
         result = result.value.args[0]
@@ -52,18 +29,18 @@ class TestCephVolumeSimpleScanModule(object):
         assert not result['stdout']
         assert not result['stderr']
 
-    @patch.object(basic.AnsibleModule, 'exit_json')
-    @patch.object(basic.AnsibleModule, 'run_command')
+    @patch('ansible.module_utils.basic.AnsibleModule.exit_json')
+    @patch('ansible.module_utils.basic.AnsibleModule.run_command')
     def test_with_failure(self, m_run_command, m_exit_json):
-        set_module_args({
+        ca_test_common.set_module_args({
         })
-        m_exit_json.side_effect = exit_json
+        m_exit_json.side_effect = ca_test_common.exit_json
         stdout = ''
         stderr = 'error'
         rc = 2
         m_run_command.return_value = rc, stdout, stderr
 
-        with pytest.raises(AnsibleExitJson) as result:
+        with pytest.raises(ca_test_common.AnsibleExitJson) as result:
             ceph_volume_simple_scan.main()
 
         result = result.value.args[0]
@@ -72,18 +49,18 @@ class TestCephVolumeSimpleScanModule(object):
         assert result['rc'] == rc
         assert result['stderr'] == stderr
 
-    @patch.object(basic.AnsibleModule, 'exit_json')
-    @patch.object(basic.AnsibleModule, 'run_command')
+    @patch('ansible.module_utils.basic.AnsibleModule.exit_json')
+    @patch('ansible.module_utils.basic.AnsibleModule.run_command')
     def test_scan_all_osds(self, m_run_command, m_exit_json):
-        set_module_args({
+        ca_test_common.set_module_args({
         })
-        m_exit_json.side_effect = exit_json
+        m_exit_json.side_effect = ca_test_common.exit_json
         stdout = ''
         stderr = ''
         rc = 0
         m_run_command.return_value = rc, stdout, stderr
 
-        with pytest.raises(AnsibleExitJson) as result:
+        with pytest.raises(ca_test_common.AnsibleExitJson) as result:
             ceph_volume_simple_scan.main()
 
         result = result.value.args[0]
@@ -94,19 +71,19 @@ class TestCephVolumeSimpleScanModule(object):
         assert result['stdout'] == stdout
 
     @patch.object(os.path, 'exists', return_value=True)
-    @patch.object(basic.AnsibleModule, 'exit_json')
-    @patch.object(basic.AnsibleModule, 'run_command')
+    @patch('ansible.module_utils.basic.AnsibleModule.exit_json')
+    @patch('ansible.module_utils.basic.AnsibleModule.run_command')
     def test_scan_path_exists(self, m_run_command, m_exit_json, m_os_path):
-        set_module_args({
+        ca_test_common.set_module_args({
             'path': fake_path
         })
-        m_exit_json.side_effect = exit_json
+        m_exit_json.side_effect = ca_test_common.exit_json
         stdout = ''
         stderr = ''
         rc = 0
         m_run_command.return_value = rc, stdout, stderr
 
-        with pytest.raises(AnsibleExitJson) as result:
+        with pytest.raises(ca_test_common.AnsibleExitJson) as result:
             ceph_volume_simple_scan.main()
 
         result = result.value.args[0]
@@ -117,14 +94,14 @@ class TestCephVolumeSimpleScanModule(object):
         assert result['stdout'] == stdout
 
     @patch.object(os.path, 'exists', return_value=False)
-    @patch.object(basic.AnsibleModule, 'fail_json')
+    @patch('ansible.module_utils.basic.AnsibleModule.fail_json')
     def test_scan_path_not_exists(self, m_fail_json, m_os_path):
-        set_module_args({
+        ca_test_common.set_module_args({
             'path': fake_path
         })
-        m_fail_json.side_effect = fail_json
+        m_fail_json.side_effect = ca_test_common.fail_json
 
-        with pytest.raises(AnsibleFailJson) as result:
+        with pytest.raises(ca_test_common.AnsibleFailJson) as result:
             ceph_volume_simple_scan.main()
 
         result = result.value.args[0]
@@ -132,21 +109,21 @@ class TestCephVolumeSimpleScanModule(object):
         assert result['rc'] == 1
 
     @patch.object(os.path, 'exists', return_value=True)
-    @patch.object(basic.AnsibleModule, 'exit_json')
-    @patch.object(basic.AnsibleModule, 'run_command')
+    @patch('ansible.module_utils.basic.AnsibleModule.exit_json')
+    @patch('ansible.module_utils.basic.AnsibleModule.run_command')
     def test_scan_path_stdout_force(self, m_run_command, m_exit_json, m_os_path):
-        set_module_args({
+        ca_test_common.set_module_args({
             'path': fake_path,
             'force': True,
             'stdout': True
         })
-        m_exit_json.side_effect = exit_json
+        m_exit_json.side_effect = ca_test_common.exit_json
         stdout = ''
         stderr = ''
         rc = 0
         m_run_command.return_value = rc, stdout, stderr
 
-        with pytest.raises(AnsibleExitJson) as result:
+        with pytest.raises(ca_test_common.AnsibleExitJson) as result:
             ceph_volume_simple_scan.main()
 
         result = result.value.args[0]
@@ -158,18 +135,18 @@ class TestCephVolumeSimpleScanModule(object):
 
     @patch.dict(os.environ, {'CEPH_CONTAINER_BINARY': fake_container_binary})
     @patch.dict(os.environ, {'CEPH_CONTAINER_IMAGE': fake_container_image})
-    @patch.object(basic.AnsibleModule, 'exit_json')
-    @patch.object(basic.AnsibleModule, 'run_command')
+    @patch('ansible.module_utils.basic.AnsibleModule.exit_json')
+    @patch('ansible.module_utils.basic.AnsibleModule.run_command')
     def test_scan_with_container(self, m_run_command, m_exit_json):
-        set_module_args({
+        ca_test_common.set_module_args({
         })
-        m_exit_json.side_effect = exit_json
+        m_exit_json.side_effect = ca_test_common.exit_json
         stdout = ''
         stderr = ''
         rc = 0
         m_run_command.return_value = rc, stdout, stderr
 
-        with pytest.raises(AnsibleExitJson) as result:
+        with pytest.raises(ca_test_common.AnsibleExitJson) as result:
             ceph_volume_simple_scan.main()
 
         result = result.value.args[0]
