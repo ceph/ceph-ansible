28cf9536ed2dec344691bddf685e95aeb9882762
Guillaume Abrioux <gabrioux@redhat.com>
2021-05-25 10:08:55 +0200

    vagrant_up: fix bash legacy syntax
    This commit rewrites the deprecated syntax used in vagrant_up.sh
    
    Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

--- tests/scripts/vagrant_up.sh
+++ tests/scripts/vagrant_up.sh
@@ -5,7 +5,7 @@ until [ $retries -ge 5 ]
 do
   echo "Attempting to start VMs. Attempts: $retries"
   timeout 10m time vagrant up "$@" && break
-  retries=$[$retries+1]
+  retries=$((retries+1))
   sleep 5
 done
 
