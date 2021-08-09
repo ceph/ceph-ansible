a36586a7777dc34cb977f81dc2d5bdfa2bebd4b6
Dimitri Savineau <dsavinea@redhat.com>
2021-08-09 13:38:26 -0400

    tests: rename grafana to monitoring
    Since the grafana-server group has been renamed to monitoring then
    changing the associated tests.
    
    Signed-off-by: Dimitri Savineau <dsavinea@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -177,8 +177,8 @@ def pytest_collection_modifyitems(session, config, items):
             item.add_marker(pytest.mark.nfss)
         elif "iscsi" in test_path:
             item.add_marker(pytest.mark.iscsigws)
-        elif "grafana" in test_path:
-            item.add_marker(pytest.mark.grafanas)
+        elif "monitoring" in test_path:
+            item.add_marker(pytest.mark.monitoring)
         else:
             item.add_marker(pytest.mark.all)
 
