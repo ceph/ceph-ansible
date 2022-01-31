67926e8d4df45d588eb92d93ac5874e93b9d3711
Guillaume Abrioux <gabrioux@redhat.com>
2022-01-31 13:42:10 +0100

    tests: use centos stream-8 instead of centos 8
    CentOS 8 is EOL as of December 2021.
    Let's use CentOS stream 8 instead.
    
    Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

--- tests/scripts/vagrant_up.sh
+++ tests/scripts/vagrant_up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-vagrant box remove --force --provider libvirt --box-version 0 centos/8 || true
-vagrant box add --provider libvirt --name centos/8 https://cloud.centos.org/centos/8/vagrant/x86_64/images/CentOS-8-Vagrant-8.4.2105-20210603.0.x86_64.vagrant-libvirt.box || true
+vagrant box remove --force --provider libvirt --box-version 0 centos/stream8 || true
+vagrant box add --provider libvirt --name centos/stream8 https://cloud.centos.org/centos/8-stream/x86_64/images/CentOS-Stream-Vagrant-8-20220125.1.x86_64.vagrant-libvirt.box || true
 
 retries=0
 until [ $retries -ge 5 ]
