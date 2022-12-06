e55ca0188190173320ca77c62d2a908e9e8f5c61
Guillaume Abrioux <gabrioux@redhat.com>
2022-12-06 13:14:07 +0100

    tests: use quay.io instead of quay.ceph.io
    This makes the CI use quay.io instead of quay.ceph.io
    
    Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

--- tests/library/test_cephadm_bootstrap.py
+++ tests/library/test_cephadm_bootstrap.py
@@ -4,9 +4,9 @@ import ca_test_common
 import cephadm_bootstrap
 
 fake_fsid = '0f1e0605-db0b-485c-b366-bd8abaa83f3b'
-fake_image = 'quay.ceph.io/ceph/daemon-base:latest-main-devel'
+fake_image = 'quay.io/ceph/daemon-base:latest-main-devel'
 fake_ip = '192.168.42.1'
-fake_registry = 'quay.ceph.io'
+fake_registry = 'quay.io'
 fake_registry_user = 'foo'
 fake_registry_pass = 'bar'
 fake_registry_json = 'registry.json'
