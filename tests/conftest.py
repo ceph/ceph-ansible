7806e515d58d8def23b49056838bdc91e90d3ec4
Teoman ONAY <tonay@ibm.com>
2024-01-15 22:27:17 +0100

    pytest-infra: fix failures
    pytest-infra 10.0.0 release changed the get_variables() behavior which led to CI failures
    (https://github.com/pytest-dev/pytest-testinfra/pull/724)
    Starting of now all groups the host is part of and not only the direct parent groups are
    returned by get_variables()
    
    Signed-off-by: Teoman ONAY <tonay@ibm.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -147,6 +147,10 @@ def node(host, request):
         'dev': 99
     }
 
+    sanitized_group_names = group_names
+    if 'all' in sanitized_group_names:
+        sanitized_group_names.remove('all')
+
     # capture the initial/default state
     test_is_applicable = False
     for marker in request.node.iter_markers():
@@ -163,7 +167,7 @@ def node(host, request):
     if request.node.get_closest_marker('rbdmirror_secondary') and not ceph_rbd_mirror_remote_user:  # noqa E501
         pytest.skip('Not a valid test for a non-secondary rbd-mirror node')
 
-    if request.node.get_closest_marker('ceph_crash') and group_names in [['nfss'], ['iscsigws'], ['clients'], ['monitoring']]:
+    if request.node.get_closest_marker('ceph_crash') and sanitized_group_names in [['nfss'], ['iscsigws'], ['clients'], ['monitoring']]:
         pytest.skip('Not a valid test for nfs, client or iscsigw nodes')
 
     if request.node.get_closest_marker("no_docker") and docker:
@@ -178,7 +182,7 @@ def node(host, request):
         pytest.skip(
             "Not a valid test with dashboard disabled")
 
-    if request.node.get_closest_marker("dashboard") and group_names == ['clients']:
+    if request.node.get_closest_marker("dashboard") and sanitized_group_names == ['clients']:
         pytest.skip('Not a valid test for client node')
 
     data = dict(
