14e5c0a7127dfc7f660d0c7c0bfdde205021d66a
Guillaume Abrioux <gabrioux@ibm.com>
2024-02-16 01:16:55 +0100

    drop iscsigw support
    This service is no longer maintained.
    Let's drop its support within ceph-ansible.
    
    Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>

--- plugins/callback/installer_checkpoint.py
+++ plugins/callback/installer_checkpoint.py
@@ -29,7 +29,6 @@ class CallbackModule(CallbackBase):
             'installer_phase_ceph_nfs',
             'installer_phase_ceph_rbdmirror',
             'installer_phase_ceph_client',
-            'installer_phase_ceph_iscsi_gw',
             'installer_phase_ceph_rgw_loadbalancer',
             'installer_phase_ceph_dashboard',
             'installer_phase_ceph_grafana',
@@ -70,10 +69,6 @@ class CallbackModule(CallbackBase):
             'installer_phase_ceph_client': {
                 'title': 'Install Ceph Client',
                 'playbook': 'roles/ceph-client/tasks/main.yml'
-            },
-            'installer_phase_ceph_iscsi_gw': {
-                'title': 'Install Ceph iSCSI Gateway',
-                'playbook': 'roles/ceph-iscsi-gw/tasks/main.yml'
             },
             'installer_phase_ceph_rgw_loadbalancer': {
                 'title': 'Install Ceph RGW LoadBalancer',
